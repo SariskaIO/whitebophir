eae7e3d7f334a528480e6159fc711616488eb764
brajendrak00068 <56827534+brajendrak00068@users.noreply.github.com>
2022-05-10 15:37:56 +0530

    Update clear.js

--- client-data/js/clear.js
+++ client-data/js/clear.js
@@ -1,1 +1,16 @@
-
+window.addEventListener('message', event => {
+    // IMPORTANT: check the origin of the data! 
+    if (event.origin.startsWith('https://meet.dev.sariska.io') || event.origin.startsWith('https://meet.sariska.io')) { 
+        // The data was sent from your site.
+        // Data sent with postMessage is stored in event.data:
+        console.log(event.data); 
+       let canvas  = document.getElementById("canvas")
+       const context = canvas.getContext('2d');
+       context.clearRect(0, 0, canvas.width, canvas.height);
+    } else {
+        // The data was NOT sent from your site! 
+        // Be careful! Do not use it. This else branch is
+        // here just for clarity, you usually shouldn't need it.
+        return; 
+    } 
+}); 
